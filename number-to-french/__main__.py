6b910e2201b64109d95f1f5898ae243625acb2e6
Zofia Smoleń <zofsmolen@gmail.com>
2024-10-05 07:37:34 +0200

    Creating a strawman of __main__.py

---
+++ number-to-french/__main__.py
@@ -0,0 +1,24 @@
+import sys
+import ast  # To safely evaluate a string as a Python literal
+from .converter import NumberToFrench
+
+def main():
+    if len(sys.argv) > 1:
+        #try:
+            # Extract the input argument, which should be a list in string format
+            
+            # Evaluate the string to a Python list using ast.literal_eval
+            
+            # Ensure the evaluated result is a list of integers
+
+            
+            # Create the NumberToFrench instance and convert the list of numbers
+
+            
+            # Format the results as a JSON-like list of strings
+
+            #Add except
+    #add a situation when numbers not provided
+    # else:
+
+        return 999
